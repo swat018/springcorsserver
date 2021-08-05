4ade98c2b5248f27367615b2e29d36d5ed0a5812
jinwoo park <swat018@icloud.com>
2021-08-05 13:14:55 +0900

    Initial commit

---
+++ src/test/java/swat018/springcorsserver/SpringcorsserverApplicationTests.java
@@ -0,0 +1,13 @@
+package swat018.springcorsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcorsserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
